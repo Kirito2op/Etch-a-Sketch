599f95967b40aff376d11c7fc50d1723172504ab
Kirito2op <thrishankreddy50@gmail.com>
2024-05-06 01:40:19 +0530

    added option of no of squares and colors

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-var container = document.getElementById('container');
+const generateBoxes = (boxes) => {
+    const cont = document.getElementById("container");
+    const color = document.getElementById("colord");
+    while(cont.firstChild){
+        cont.removeChild(cont.firstChild);
+    }
+    const noOfBoxes = 500/boxes;
+    for(var i=0;i<boxes*boxes;i++){
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("box");
+        newDiv.style.width = noOfBoxes + "px";
+        newDiv.style.height = noOfBoxes + "px";
+        newDiv.addEventListener("mouseenter",() => {
+            newDiv.style.backgroundColor = color.value;
+        });
+        cont.appendChild(newDiv);
+    }
+};
 
-for (var i = 0; i < 16 * 16; i++) {
-    var box = document.createElement('div');
-    box.classList.add('box');
-    container.appendChild(box);
-    box.addEventListener('mouseenter', function(event) {
-        event.target.classList.add('red');
-    });
-}
+generateBoxes(5);
+
+const pixel = document.getElementById("pixeld");
+
+pixel.addEventListener("change",function(){
+    generateBoxes(pixel.value);
+});
+const resetAll = (() => {
+    const cont = document.getElementById("container");
+    const boxes = cont.children;
+    for(let i=0;i<boxes.length;i++){
+        boxes[i].style.backgroundColor = "white";
+    }
+}); 
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener("click",() =>{
+    resetAll();
+});
